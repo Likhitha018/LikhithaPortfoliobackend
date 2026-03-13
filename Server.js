45615aebdff353795af183d8fd2e58ede8c9e3b6
Likhitha-Thangellapally <likhitha043@gmail.com>
2026-03-13 19:39:30 +0530

    portfolio backend

--- Server.js
+++ Server.js
@@ -7,21 +7,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// MySQL connection
-const db = mysql.createConnection(
-"mysql://root:qrQEJvFvJvVXVUiMsgEfZFJLGuMdeOyx@yamabiko.proxy.rlwy.net:32643/railway"
-)
+const db = mysql.createConnection(process.env.DATABASE_URL)
 
 db.connect((err)=>{
 if(err){
 console.log("Database connection failed",err)
-}
-else{
+}else{
 console.log("Connected to Railway")
 }
 })
 
-// API to save contact form
 app.post("/contact",(req,res)=>{
 
 const {name,email,message} = req.body
@@ -38,6 +33,8 @@ res.send("Message saved successfully")
 
 })
 
-app.listen(5000,()=>{
-console.log("Server running on port 5000")
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+console.log("Server running")
 })
